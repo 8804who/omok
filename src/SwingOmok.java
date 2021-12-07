bcbd918bd7d4d347b2b1ba415d0b7e84392434e9
1a2a3a4a5 <8804who@naver.com>
2021-12-07 14:18:03 +0900

    스윙으로 오목 만들기

--- src/SwingOmok.java
+++ src/SwingOmok.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class SwingOmok extends JFrame{
     Container c = getContentPane();
     JLayeredPane layer = new JLayeredPane();
+    JPanel stonePane = new JPanel();
 
     ImageIcon bg=new ImageIcon("img/board.jpg");//바둑판 이미지
     ImageIcon black = new ImageIcon("img/black.jpg");//흑돌 이미지
@@ -25,7 +26,7 @@ public class SwingOmok extends JFrame{
 
         setSize(1020, 1050);
         setVisible(true);
-        setResizable(false);
+        setResizable(false);//이미지 크기에 창을 맞추어놓았으므로 창 크기 변경 불가 설정
     }
     public void setLayerPane(){
         c.add(layer);
@@ -53,40 +54,41 @@ public class SwingOmok extends JFrame{
         layer.removeAll();
     }
 
-    public void makeStone(int X, int Y){
+    public void makeStone(int X, int Y){//클릭한 위치에 바둑돌을 배치
         JLabel stone = new JLabel();
         if(map[X][Y]==0){
             if(turn.equals("black")) {
-                map[X][Y]=1;
+                map[X][Y]=1;//돌의 위치를 저장하는 배열에 돌의 종류 저장
                 stone.setIcon(black);
             }
             else{
-                map[X][Y]=2;
+                map[X][Y]=2;//돌의 위치를 저장하는 배열에 돌의 종류 저장
                 stone.setIcon(white);
             }
-            layer.add(stone,new Integer(100));//돌을 배경 위에 배치
+            stonePane.add(stone,new Integer(100));//돌을 배경 위에 배치
             stone.setSize(50,50);
             stone.setLocation(X*52, Y*52);
+            layer.add(stone,new Integer(100));
         }
     }
 
-    public void getResult(int X, int Y){
+    public void getResult(int X, int Y){//클릭한 위치를 중심으로 주변을 탐색해서 승리조건에 알맞는지 확인
         int[] straight_Num=new int[8];
         for(int i=0;i<8;i++){
             int count=0;
             for(int j=1;j<=5;j++){
-                if(X+move[i][0]*j>0 & Y+move[i][1]*j>0 & X+move[i][0]*j<19 & Y+move[i][1]*j<19){
-                    if(map[X+(move[i][0])*j][Y+(move[i][1])*j]==map[X][Y]) {
+                if(X+move[i][0]*j>0 & Y+move[i][1]*j>0 & X+move[i][0]*j<19 & Y+move[i][1]*j<19){//탐색범위가 맵을 벗어나는것을 방지해주기 위해 범위를 설정
+                    if(map[X+(move[i][0])*j][Y+(move[i][1])*j]==map[X][Y]) {//탐색하는 위치에 있는 돌이 자신이 놓은 돌과 같은 돌이면 count++
                         count++;
                     }
-                    else break;
+                    else break;//탐색하는 위치에 있는 돌이 자신이 놓은 돌과 다르면 그 방향으로는 탐색 종료
                 }
                 else break;
             }
             straight_Num[i]=count;
         }
 
-        if((straight_Num[0]+straight_Num[1]>=4) || (straight_Num[2]+straight_Num[3]>=4)|| (straight_Num[4]+straight_Num[5]>=4)|| (straight_Num[6]+straight_Num[7]>=4)) {
+        if((straight_Num[0]+straight_Num[1]>=4) || (straight_Num[2]+straight_Num[3]>=4)|| (straight_Num[4]+straight_Num[5]>=4)|| (straight_Num[6]+straight_Num[7]>=4)) {//상+하,좌+우,좌상+우하,좌하+우상을 하여서 4이상이 되면 현재 돌과 합쳐서 5
             gameEnd=true;
             if(turn.equals("black")) JOptionPane.showMessageDialog(null, "흑의 승리입니다.","게임 종료",JOptionPane.PLAIN_MESSAGE);
             else JOptionPane.showMessageDialog(null, "백의 승리입니다.","게임 종료",JOptionPane.PLAIN_MESSAGE);
@@ -95,12 +97,18 @@ public class SwingOmok extends JFrame{
 
     class ClickListener extends MouseAdapter{
         public void mouseClicked(MouseEvent e) {
-            int X=e.getX()-30, Y=e.getY()-30;
-            if(X>=0 & Y>=0 & X<=970 & Y<=970){
-                makeStone(X/50,Y/50);
-                getResult(X/50, Y/50);
-                if(turn.equals("black")) turn="white";//한번 진행할때 마다 순서 변경
-                else turn="black";
+            int X=e.getX()-30, Y=e.getY()-30;//바둑판의 안쓰는 부분제거
+            if(X>=0 & Y>=0 & X<=970 & Y<=970 & map[X/50][Y/50]==0){//바둑판의 칸에만 반응하도록 범위 설정 & 돌이 놓이지 않은 곳만 지정 가능
+                makeStone(X/50,Y/50);//바둑판의 칸에 돌을 놓기
+                getResult(X/50, Y/50);//현재 놓은 칸 근처를 탐색
+                if(turn.equals("black")) {
+                    JOptionPane.showMessageDialog(null, "백의 차례입니다.","차례 변경",JOptionPane.PLAIN_MESSAGE);
+                    turn="white";//한번 진행할때 마다 순서 변경
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "흑의 차례입니다.","차례 변경",JOptionPane.PLAIN_MESSAGE);
+                    turn="black";
+                }
             }
         }
     }
